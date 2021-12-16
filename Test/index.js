901df4b89c0b7c5731d443c55f6a6901f9c459e4
Nicola <51528817+NikoMrs@users.noreply.github.com>
2021-12-16 11:43:19 +0100

    Quinta versione
    Rimosso il file users.json in quanto non usato.
    Ritocchi minimi ai file index.js delle cartelle api e test

--- Test/index.js
+++ Test/index.js
@@ -9,69 +9,9 @@ test('Test 1: Annunci ritornati correttamente', function (assert) {
         .expect('Content-Type', /json/)     //array?
         .expect(200)
         .end(function (err, res) {
-            /*
-            var expectedUsers = [
-              {
-                "_id": {
-                  "$oid": "61b1b99895c880c19dab8a33"
-                },
-                "_idProprietario": {
-                  "$oid": "313233343536373839303132"
-                },
-                "titolo": "Dogsitter a Trento",
-                "descrizione": "Offro servizi di dogsitting nei pressi di Trento e dintorni",
-                "costo": "5",
-                "luogo": "Trento",
-                "categoria": "AnimaliDomestici",
-                "foto": "foto",
-                "voti": [
-                  "4",
-                  "5"
-                ],
-                "segnalazioni": 0,
-                "sponsorizzato": true,
-                "nascosto": false
-              },{
-                "_id": {
-                  "$oid": "61b1c0baa1f86a1ede42740d"
-                },
-                "_idProprietario": {
-                  "$oid": "61af3a1ed80d6205e1439553"
-                },
-                "titolo": "Curo il tuo giardino",
-                "descrizione": "Servizi di giardinaggio per terreni di piccole-medie dimensioni",
-                "costo": "7.5",
-                "luogo": "Civezzano",
-                "categoria": "Giardinaggio",
-                "foto": "foto",
-                "voti": [],
-                "segnalazioni": 0,
-                "sponsorizzato": false,
-                "nascosto": false
-              },{
-                "_id": {
-                  "$oid": "61b1c150a1f86a1ede42740e"
-                },
-                "_idProprietario": {
-                  "$oid": "61af3a1ed80d6205e1439553"
-                },
-                "titolo": "Falciatura dei campi",
-                "descrizione": "Offro servizi di giardinaggio per terreni di grandi dimensioni",
-                "costo": "10",
-                "luogo": "Borgo Valsugana",
-                "categoria": "Giardinaggio",
-                "foto": "foto",
-                "voti": [],
-                "segnalazioni": 0,
-                "sponsorizzato": false,
-                "nascosto": false
-              }
-            ];
-            */
 
             var isNotZero = false;
             var count = res.body.length;
-            //console.log("\n\n\n\nNumero el: " + count);
             
             if(count > 0){
               isNotZero = true;
@@ -89,7 +29,7 @@ test('Test 2: Annuncio aggiunto correttamente', function (assert) {
   request(app)
       .post('/api/annunci/test')
       .send({
-        "_idProprietario": "000000000000000000000000", "titolo": "TitoloTesting", "descrizione": "Testing",
+        "_idProprietario": "000000000000000000000001", "titolo": "TitoloTesting", "descrizione": "Testing",
         "costo": "0", "luogo" : "LuogoTesting", "categoria" : "CategoriaTesting", "foto" : "FotoTesting", "voti" : [], 
         "segnalzioni" : 0, "sponsorizzato" : false, "nascosto" : "false"
       })
--- api/index.js
+++ api/index.js
@@ -58,71 +58,6 @@ app.get('/api/annunci', (request, response) => {                                
 
 })
 
-/*
-app.get('/api/annunci/test2', (request, response) => {                                    //Ritorna annunci test
-    
-    var result = [
-        {
-            "_id": {
-            "$oid": "61b1b99895c880c19dab8a33"
-            },
-            "_idProprietario": {
-            "$oid": "313233343536373839303132"
-            },
-            "titolo": "Dogsitter a Trento",
-            "descrizione": "Offro servizi di dogsitting nei pressi di Trento e dintorni",
-            "costo": "5",
-            "luogo": "Trento",
-            "categoria": "AnimaliDomestici",
-            "foto": "foto",
-            "voti": [
-            "4",
-            "5"
-            ],
-            "segnalazioni": 0,
-            "sponsorizzato": true,
-            "nascosto": false
-        },{
-            "_id": {
-            "$oid": "61b1c0baa1f86a1ede42740d"
-            },
-            "_idProprietario": {
-            "$oid": "61af3a1ed80d6205e1439553"
-            },
-            "titolo": "Curo il tuo giardino",
-            "descrizione": "Servizi di giardinaggio per terreni di piccole-medie dimensioni",
-            "costo": "7.5",
-            "luogo": "Civezzano",
-            "categoria": "Giardinaggio",
-            "foto": "foto",
-            "voti": [],
-            "segnalazioni": 0,
-            "sponsorizzato": false,
-            "nascosto": false
-        },{
-            "_id": {
-            "$oid": "61b1c150a1f86a1ede42740e"
-            },
-            "_idProprietario": {
-            "$oid": "61af3a1ed80d6205e1439553"
-            },
-            "titolo": "Falciatura dei campi",
-            "descrizione": "Offro servizi di giardinaggio per terreni di grandi dimensioni",
-            "costo": "10",
-            "luogo": "Borgo Valsugana",
-            "categoria": "Giardinaggio",
-            "foto": "foto",
-            "voti": [],
-            "segnalazioni": 0,
-            "sponsorizzato": false,
-            "nascosto": false
-        }
-    ]
-
-    response.json(result);
-    
-})
-*/
 app.get('/api/annunci/test', (request, response) => {                                    //Ritorna annunci test
 
     MongoClient.connect(CONNECTION_STRING, { useNewUrlParser: true }, (error, client) => {
@@ -133,7 +68,6 @@ app.get('/api/annunci/test', (request, response) => {                           
                 console.log(error);
             }
 
-            //console.log("\n\n\n\nNumero elementi: " + result.length);
             result = JSON.parse(JSON.stringify(result));
             response.status(200).send(result);
         });
